0e56cc6c697d7ff2311a287a5ff18035e6656a16
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2024-01-25 13:54:23 +0500

    Worked on functionality for the feedback form

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-  echo "Hello";  
+  try{
+    $connection = mysqli_connect("localhost", "root", "", "artattack");
+  }
+  catch(mysqli_sql_exception){echo "Not Connected <br>";}
+  
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $email = filter_input(INPUT_POST, "e-mail", FILTER_SANITIZE_SPECIAL_CHARS);
+    $fback = filter_input(INPUT_POST, "feedback", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $sqlQ = "INSERT INTO feedback (E-mail, feedback) VALUES ('$email', '$fback')";
+
+    mysqli_query($connection, $sqlQ);
+
+  }
+
+  mysqli_close($connection);
 ?>
